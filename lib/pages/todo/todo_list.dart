0e5ae90c02db68151aa24ea2ba6d0faee0b658df
Tuan Nguyen <me@tuannguyen.me>
2018-09-27 06:56:30 +0700

    - Clean up initial code - Add TodoListPage

---
+++ lib/pages/todo/todo_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Todo list'),
+    );
+  }
+}
