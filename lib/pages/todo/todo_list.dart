6c4ea3c6ddb403d27309de6bcd7ae94639744ba5
Tuan Nguyen <me@tuannguyen.me>
2018-09-27 08:52:21 +0700

    Scaffold TotoListPage with floating action button

--- lib/pages/todo/todo_list.dart
+++ lib/pages/todo/todo_list.dart
@@ -3,8 +3,17 @@ import 'package:flutter/material.dart';
 class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Todo list'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Center(
+        child: Text('Todo list'),
+      ),
     );
   }
 }
