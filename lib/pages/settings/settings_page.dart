7f5813c838da9b387d54abc12f8c926a84f51e26
Tuan Nguyen <me@tuannguyen.me>
2018-10-19 10:52:45 +0700

    Remove redundant import

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 import 'package:flutter_todo/widgets/ui_elements/loading_modal.dart';
-import 'package:flutter_todo/.env.dart';
 import 'package:flutter_todo/scoped_models/app_model.dart';
 import 'package:flutter_todo/widgets/helpers/confirm_dialog.dart';
 
